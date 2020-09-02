bb92476ceae264e642a0419b956acfb31fb1e8ec
Matheus Mazoni de Andrade <mmazoni.andrade@gmail.com>
2020-09-01 22:33:28 -0300

    Adding first structure of home page with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!--Navbar-->
+    <div class="pos-f-t">
+      <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+          <h5 class="text-white h4">Collapsed content</h5>
+          <span class="text-muted">Toggleable via the navbar brand.</span>
+        </div>
+      </div>
+      <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+    </div>
+    <!--End Navbar-->
+
+    <!--Sidebar-->
+    <div class="sidebar">
       <router-link to="/">Home</router-link>
     </div>
-    <router-view/>
+    <!--End Sidebar-->
+
+    <!--Main content-->
+    <div class="wrapper">
+      <router-view/>
+    </div>
+    <!--End Main content-->
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
