dfee4c06367b441db4eff905fb34f031ddd3b3f8
Matheus Mazoni <mmazoni.andrade@gmail.com>
2020-09-06 17:20:17 -0300

    dashboard

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,47 @@
 <template>
-    <nav>
-        <v-app-bar text app>
-            <v-toolbar-title class="text-uppercase grey--text">
-                <span class="font-weight-light">Finan</span>
-                <span>Place</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn text color="grey">
-                <span>Sair</span>
-                <v-icon right>exit_to_app</v-icon>
-            </v-btn>
-        </v-app-bar>    
-    </nav>    
+  <nav>
+    <v-app-bar text app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer">
+        <v-icon>menu</v-icon>
+      </v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Finen</span>
+        <span>Place</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text color="grey">
+        <span>Sair</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="yellow lighten-1">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
 export default {
-    name: "Navbar"
-}
+  name: "Navbar",
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {icon: 'dashboard', text: 'Dashboard', route: '/'},
+        {icon: 'folder', text: 'Caixa', route: '/caixa'},
+        {icon: 'person', text: 'Funcionários', route: '/pessoas'}        
+      ]
+    };
+  },
+};
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,5 +6,14 @@ Vue.use(Vuetify);
 export default new Vuetify({
     icons: {
         iconfont: 'mdi'
-    }
+    },
+    theme: {
+
+            primary: '#067bc2',
+            light_blue: '#ecc30b',
+            secondary: '#ecc30b',
+            dark_gray: '#393e41',
+            light_gray: '#d3d0cb'
+        }
+    
 });
