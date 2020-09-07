5bfa8f9d3a1f177f1c21efc2b6395d3af191746f
Matheus Mazoni <mmazoni.andrade@gmail.com>
2020-09-07 10:09:56 -0300

    caixa service

---
+++ src/services/caixa.js
@@ -0,0 +1,32 @@
+
+export const getCaixas = () =>  axios.get("caixa");
+
+export const showCaixa = id => axios.get(`caixa/${id}`);
+
+export const getControleCaixas = () => axios.get("controle");
+
+export const showControleCaixa = id => axios.get(`controle/${id}`);
+
+export const storeControleCaixa = () => axios.post("controle");
+
+export const storeCaixa = data => {
+    console.log(data);
+    return axios.post("caixa/", {
+      cd_ControleCaixa: data.cd_ControleCaixa,
+      vl_CaixaInicial: data.vl_CaixaInicial
+    });
+  }
+  
+  export const fecharCaixa = (id, data) => {
+    return axios.patch(`caixa/${id}/`, {
+      vl_Dinheiro: data.dinheiro,
+      vl_CartaoCredito: data.credito,
+      vl_CartaoDebito: data.debito,
+      vl_Refeicao: data.refeicao,
+      vl_Online: data.online,
+      vl_Sangrias: data.sangrias,
+      vl_Despesas: data.despesas,
+      vl_Entradas: data.entradas,
+      vl_Faturamento: data.faturamento
+    });
+}
