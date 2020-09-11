e4fcd50f219848bcd96da4d3472552ba5233bed2
Matheus Mazoni <mmazoni.andrade@gmail.com>
2020-09-11 17:33:22 -0300

    criando as listas dinamicamente

--- src/views/CaixaAberto.vue
+++ src/views/CaixaAberto.vue
@@ -3,10 +3,10 @@
     <v-subheader class="grey--text">Caixa Aberto</v-subheader>
     <v-container class="my-5 pt-5">
       <v-row>
-        <v-col cols="11" md="3">
+        <v-col cols="11" md="3" class="mr-5 pr-5">
           <v-form>
             <v-text-field v-model="entrada" label="Entrada"></v-text-field>
-            <v-btn text icon color="secondary">
+            <v-btn text icon color="secondary" @click="addToList('entrada')">
               <v-icon>add</v-icon>
             </v-btn>
           </v-form>
@@ -16,11 +16,11 @@
           <v-divider vertical></v-divider>
         </v-col>
 
-        <v-col cols="11" md="3">
+        <v-col cols="11" md="3" class="mr-5 pr-5">
           <v-form>
             <v-select v-model="tipo" :items="tipos" label="Tipo" required></v-select>
             <v-text-field v-model="despesa" label="Despesa"></v-text-field>
-            <v-btn text icon color="secondary">
+            <v-btn text icon color="secondary" @click="addToList('despesa')">
               <v-icon>add</v-icon>
             </v-btn>
           </v-form>
@@ -30,26 +30,22 @@
           <v-divider vertical></v-divider>
         </v-col>
 
-        <v-col cols="11" md="3">
+        <v-col cols="12" md="3" class="mr-5 pr-5">
           <v-form>
             <v-text-field v-model="sangria" label="Sangria"></v-text-field>
-            <v-btn text icon color="secondary">
+            <v-btn text icon color="secondary" @click="addToList('sangria')">
               <v-icon>add</v-icon>
             </v-btn>
           </v-form>
         </v-col>
 
-        <v-col cols="1">
-          <v-divider vertical></v-divider>
-        </v-col>
       </v-row>
       <v-btn block color="secondary" class="mb-5">Preparar fechamento</v-btn>
-      <v-divider></v-divider>
       <v-row>
         <v-col cols="12">
           <v-list v-for="item in items" :key="item.id">
             <div class="d-flex justify-end py-0 my-0 border-bottom">
-              <v-list-item class="py-0 my-0">{{ item.id }} - {{ item.categoria }} | {{ item.valor }}</v-list-item>
+              <v-list-item class="py-0 my-0">{{ item.id + 1 }} - {{ item.categoria }} | R$ {{ item.valor }}</v-list-item>
               <v-list-item-action class="py-0 my-0">
                 <v-btn icon color="red" class="mr-3">
                   <v-icon>remove_circle_outline</v-icon>
@@ -74,13 +70,15 @@ export default {
       { value: 2, text: "Petit Cash" },
       { value: 3, text: "Esqueci" },
     ],
-    items: [
-      { id: 1, categoria: "Entrada", valor: "R$ 1000,00" },
-      { id: 2, categoria: "Entrada", valor: "R$ 1000,00" },
-      { id: 3, categoria: "Entrada", valor: "R$ 1000,00" },
-    ],
+    items: [],
     despesa: "",
     sangria: "",
   }),
+  methods: {
+    addToList(category) {
+      let id = this.items.length;
+      this.$set(this.items, id, {id: id, categoria: category, valor: this[category]});
+    }
+  }
 };
 </script>
